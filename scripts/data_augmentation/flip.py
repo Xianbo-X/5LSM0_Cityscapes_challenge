d7dbcbd8ddce8527c7cb2dd0049f150e77814fd9
Xianbo <x.xu@student.tue.nl>
2022-04-10 18:16:42 +0200

    feat(data_aug): implementation flip for both img and mask
    Implementation flipping class that process image and mask simulaneously

--- scripts/data_augmentation/flip.py
+++ scripts/data_augmentation/flip.py
@@ -1,4 +1,5 @@
 import torchvision.transforms.functional as TF
+import random
 
 class HorizontalFlip():
     """
@@ -40,3 +41,50 @@ class VerticalFlip():
         """
         return TF.vflip(img)
         
+class RandomHorizontalFlip2():
+    """
+    Flip the given image and mask horizontally,
+    """
+
+    def __init__(self,pr=0.5) -> None:
+        self.pr=pr
+
+    def __call__(self, img_mask):
+        """
+        Parameters:
+        ----------
+            img: PIL Image or Tensor
+            mask: PIL Image or Tensor
+
+        Returns:
+        ---------
+            Flipped image, Flipped mask
+        """
+        res=img_mask
+        if random.random()< self.pr:
+            res=(TF.hflip(img_mask[0]),TF.hflip(img_mask[1]))
+        return res
+
+class RandomVerticalFlip2():
+    """
+    Flip the given image vertically
+    """
+
+    def __init__(self,pr=0.5) -> None:
+        self.pr=pr
+
+    def __call__(self, img_mask):
+        """
+        Parameters:
+        ----------
+            img: PIL Image or Tensor
+            mask: PIL Image or Tensor
+
+        Returns:
+        ---------
+            Flipped image, Flipped mask
+        """
+        res=img_mask
+        if random.random()< self.pr:
+            res=(TF.vflip(img_mask[0]),TF.vflip(img_mask[1]))
+        return res
