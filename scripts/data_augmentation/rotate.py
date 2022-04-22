7965b335c650ff275dc583e95419c5b9ab7093f9
Xianbo XU <x.xu@student.tue.nl>
2022-04-22 17:33:59 +0200

    refactor(augmentation): add init parameter pr for RandomRotation

--- scripts/data_augmentation/rotate.py
+++ scripts/data_augmentation/rotate.py
@@ -5,7 +5,8 @@ class RandomRotation():
     """
     Rotate the given image with random angle
     """
-    def __init__(self) -> None:
+    def __init__(self,pr) -> None:
+        self.pr=pr
         pass
 
     def __call__(self, img_mask):
@@ -19,6 +20,7 @@ class RandomRotation():
             Rotated image
         """
         # randomly generate rotation angle
-        angle = random.randint(0,180)
-        
-        return TF.rotate(img_mask[0], angle=angle), TF.rotate(img_mask[1], angle=angle)
+        if random.random()<self.pr:
+            angle = random.randint(0,180)
+            return TF.rotate(img_mask[0], angle=angle), TF.rotate(img_mask[1], angle=angle)
+        return img_mask
