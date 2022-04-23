d17b748f4e3b0bc714653fe631c4bf1438e309b5
Xianbo XU <x.xu@student.tue.nl>
2022-04-23 11:01:01 +0200

    feat(run): accept folder paths as args

--- run_config.py
+++ run_config.py
@@ -1,13 +1,18 @@
+from ast import parse
 import os
 from tqdm import tqdm
 import numpy as np
+import argparse
 
 if __name__=="__main__":
-    run_name=["unet_1","unet_2","unet_2_1","res_unet","pos_unet"]
-
+    # run_name=["unet_1","unet_2","unet_2_1","res_unet","pos_unet"]
+    parser=argparse.ArgumentParser("Run multiple configs")
+    parser.add_argument("--config",help="config folder name",nargs="+")
+    args=parser.parse_args()
+    print(args.config)
     cmds=[]
-    for  model in run_name:
-        folder=os.path.join("config",model)
+    for  folder in args.config:
+        # folder=os.path.join("config",model)
         configurations=map(lambda x:os.path.join(folder,x),os.listdir(folder))
         cmds_one_model=[]
         for config in configurations:
@@ -15,8 +20,9 @@ if __name__=="__main__":
             cmd=f"python main.py --path {config}"
             cmds_one_model.append(cmd)
             # print(cmd)
-            # os.system(cmd)
+    #         # os.system(cmd)
         cmds.append(cmds_one_model)
+    # print(cmds)
     cmds=np.array(cmds).T.flatten()
     for cmd in tqdm(cmds):
         os.system(cmd)
